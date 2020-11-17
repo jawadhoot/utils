35f9c4a85f728ee3bcdc3c66e2d9b5f7089d2ce1
Awadhoot Jog <jawadhoot@gmail.com>
2020-11-17 11:11:44 +0530

    cleaner output

--- git-clone-pull.py
+++ git-clone-pull.py
@@ -5,12 +5,15 @@ url = "https://github.com/jawadhoot/"
 repos = ["aatp", "dotcom" , "knockoffmania", "pibot", "pixel-art", "resume", "story-board"]
 workspace = Path.home().joinpath("workspace")
 print(str(workspace))
-os.system("mkdir " + str(workspace))
+if not Path.is_dir(workspace):
+  os.mkdir(str(workspace))
 
 for repo in repos:
-  repo_dir = str(workspace.joinpath(repo))
+  repo_dir = workspace.joinpath(repo)
   print(repo_dir)
-  os.system("git clone " + url + repo + " " + repo_dir)
+  os.chdir(workspace)
+  if not Path.is_dir(repo_dir.joinpath(".git")):
+    os.system("git clone " + url + repo)
   os.chdir(repo_dir)
   os.system("git pull")
   os.system("git push")
